4a310da9712dff512dd13ad3047388afd4bf2ded
kelvin <myemail@gmail.com>
2023-03-18 05:36:20 -0700

    puts hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+<include stdio.h>
+/* Entry point of the funtion
+ * Main block
+ * end of the function return 
+*/
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
