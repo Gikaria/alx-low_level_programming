c02927982e3c5dc4c21c21a7b30b9f803794142d
kelvin Gitonga <100723539+Gikaria@users.noreply.github.com>
2023-03-18 21:19:01 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *Entry point of the funtion
- *Main block
- *end of the function return. 
-*/
+ * Entry point of the funtion
+ * Main block
+ * end of the function return. 
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
