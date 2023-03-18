0cd778dc127957b9e56c5efb0d657266f1d2ebcf
kelvin <myemail@gmail.com>
2023-03-18 15:19:14 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * description: main is the entry point
+ * printf: prints out the line 
+ * return: 0 always in sucess
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
