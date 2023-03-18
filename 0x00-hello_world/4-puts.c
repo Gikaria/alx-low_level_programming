a876d26c16f143b212bbd9ee0c442afeff209bf3
kelvin <myemail@gmail.com>
2023-03-18 06:17:09 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/* Entry point of the funtion
- * Main block
- * end of the function return 
+/**
+ *Entry point of the funtion
+ *Main block
+ *end of the function return. 
 */
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
