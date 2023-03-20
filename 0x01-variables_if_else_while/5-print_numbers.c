a423622398e232068c697bcc9f0a254e097f9bd1
kelvin <myemail@gmail.com>
2023-03-20 05:06:42 -0700

    print out numbers form 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print base 10 numbers  starting from 0'
+ * Return: 0 always (sucess)
+ */
+int main(void)
+{
+	int t;
+
+	for (t = 0; t < 10; t++)
+	{
+		printf("%d", t);
+	}
+	printf("\n");
+	return (0);
+}
