49c9797a0ac2692d4ce2904a1bdd98cf18cb56ec
kelvin <myemail@gmail.com>
2023-03-22 10:57:03 -0700

    add two positive integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * int - Te program will add inegers only
+ * Return: ...
+ */
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+	return (result);
+}
