5a12ddb29c3fa9a88aea18ea3e80e14523d96e60
kelvin <myemail@gmail.com>
2023-03-23 09:14:56 -0700

    check for digit between 0 and 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit -function that checks for digits
+ * @c: character to be checked
+ * Return: 0 Success
+ */
+int _isdigit(int c)
+{
+	if ((c >= (0 + '0')) && (c <= (9 +'0')))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
