084fea8d6c3a35e1d7a260c0afcd0969e653fc81
kelvin Gitonga <100723539+Gikaria@users.noreply.github.com>
2023-03-23 12:29:26 +0300

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,9 +6,10 @@
  */
 int main(void)
 {
-	int a1 = 1, a2 = 2, a3, n;
-	
+	unsigned long a1 = 1, a2 = 2, a3; 
+	int n;
 	printf("%d, %d, ", a1, a2);
+	
 	for (n = 0; n < 50; ++n)
 	{
 		a3 = a1 + a2;
