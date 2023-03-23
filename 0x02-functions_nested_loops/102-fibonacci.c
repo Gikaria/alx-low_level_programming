6daa6c46a2b903ee04479c8401ab6e87c3812af4
kelvin <myemail@gmail.com>
2023-03-23 01:36:55 -0700

    print out first 50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: print out fibonacci for firts 50 numbers
+ * Return: 0 success
+ */
+int main(void)
+{
+	int a1 = 1, a2 = 2, a3, n;
+
+	for (n = 0; n < 50; n++)
+	{
+		a3 = a1 + a2;
+		printf("%d", a3);
+		a1 = a2;
+		a2 = a3;
+
+		if (n == 49)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(",");
+		}
+	}
+	return (0);
+}
