6a6fd1d5771ca6788f759908b55fa1d97f317967
kelvin <myemail@gmail.com>
2023-03-23 03:25:40 -0700

    print sum of even numbers fibonacci numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints out the sum of fibonacci numbers not exceeding 4,000,00
+ * Return: 0 success
+ */
+int main(void)
+{
+	unsigned long a1 = 1, a2 = 2, a3;
+	float sum = 0.00;
+
+	while (a3 < 4000000)
+	{
+		a3 = a1 + a2;
+		if ((a3 % 2) == 0)
+		{
+			sum += a3;
+		}
+		a1 = a2;
+		a2 = a3;
+	}
+	printf("%.0f\n", sum);
+	return (0);
+}
+
+
