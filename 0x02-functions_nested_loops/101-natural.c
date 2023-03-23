dd2c90a9ec3fd437652d53dd2c066f7d228b00c8
kelvin <myemail@gmail.com>
2023-03-23 00:34:10 -0700

    print sum of multiple of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: print out sum of multipes of three and 5
+ * Return: 0 success
+ */
+int main(void)
+{
+	int n, sum = 0;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if ((n % 3) == 0 || (n % 5) == 0)
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", n);
+	return (0);
+}
