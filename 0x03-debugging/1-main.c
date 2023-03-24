22ee9de75f4aa07d96ae6ca5306b2d5642f1efbc
kelvin <myemail@gmail.com>
2023-03-24 09:41:45 -0700

    update

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,9 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
+
 	/**
 	 * while (i < 10)
 	 * {
@@ -16,5 +18,7 @@ int main(void)
 	 * }
 	 */
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
+
 }
