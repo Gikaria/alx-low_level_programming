5dbbb75a99b5efab1cdfe10babbb47fdae3b895d
kelvin <myemail@gmail.com>
2023-03-30 03:02:12 -0700

    update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,12 +5,14 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0, len = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i++])
+		len++;
+
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
-		if (str[i] % 2 == 0)
-			_putchar(str[i]);
+		_putchar(str[i]);
 
 	}
 	_putchar('\n');
