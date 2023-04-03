dc2aa9743673a71d3713264835cb9f2887f98c45
kelvin <myemail@gmail.com>
2023-04-03 13:03:39 -0700

    update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,12 +8,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned  int a;
 
-	while (n > 0)
+	a = 0;
+	while (a < n)
 	{
-		*s = b;
-		s++;
-		n--;
+		s[a] = b;
+		a++;
 	}
 	return (s);
 }
