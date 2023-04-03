80a128bcba51291feec63bc955044daa30d137bd
kelvin <myemail@gmail.com>
2023-04-03 13:58:19 -0700

    locate a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: the string to be searched
+ * @c: the character to search
+ * Return: s if sucess ,'\0'if not
+ */
+char *_strchr(char *s, char c)
+{
+	int n;
+
+	n = 0;
+	while (s[n] != '\0')
+	{
+		if (s[n] == c)
+			return (s + n);
+
+		n++;
+	}
+	return ('\0');
+}
