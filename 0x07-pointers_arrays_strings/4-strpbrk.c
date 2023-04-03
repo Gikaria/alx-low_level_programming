3cce7b2d3dd0f9972a125406ee2dbd5be20d8d63
kelvin <myemail@gmail.com>
2023-04-03 15:19:25 -0700

    search for any set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strpbrk - searches for any set of bytes between s and accept
+ * @s: the string suspected to have similar bytes with accept
+ * @accept: the string suspected to have similar bytes with s
+ * Return: s  or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+			if (*s == accept[i])
+				return (s);
+	
+		s++;
+	}
+	return ('\0');
+}
