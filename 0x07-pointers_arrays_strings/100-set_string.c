27dc34e29e0a27292339bf292cb7f7b13f44bd2a
kelvin <myemail@gmail.com>
2023-04-04 05:07:40 -0700

    pointer to pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string- sets the value of pointer to a char
+ * @s: pointer to pointer char *to
+ * @to: pointer of type char
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
