18db0c5f878c6fefca4c9e3ed1dd67b264d33200
kelvin <myemail@gmail.com>
2023-04-11 06:26:22 -0700

    update

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -2,13 +2,14 @@
 /**
  * _atoi - change character string to integer
  * @s: the string to convert to integer
+ * Return: if success.
  */
 int _atoi(char *s)
 {
 	int i, result;
-	result = 0;
 
-	for(i = 0; s[i] != '\0'; i++)
+	result = 0;
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		result = result * 10 +  s[i] - '0';
 	}
