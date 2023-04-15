378ddc59463af8ca3679e40106ecef70b7c08c0b
kelvin <myemail@gmail.com>
2023-04-15 13:51:15 -0700

    it ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * function prints the name of the program
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("Program name is: %s\n",*argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef Header_file
+#define Header_file
+
+int _putchar(char c);
+int _atoi(char *s);
+
+#endif
