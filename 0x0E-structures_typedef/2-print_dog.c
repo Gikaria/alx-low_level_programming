159de66358432b21c3408599df7e0cf3fcedf67b
kelvin <myemail@gmail.com>
2023-04-18 14:10:47 -0700

    update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,11 +7,10 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d)
 	{
-		printf("Name: (nil)");
-	}
-	printf("Name: %s\n", (*d).name);
+	printf("Name: %s\n", (*d).name ? (*d).name : "(nil)");
 	printf("Age: %f\n", (*d).age);
-	printf("Owner: %s\n", (*d).owner);
+	printf("Owner: %s\n", (*d).owner ? (*d).owner : "(nil)");
+	}
 }
