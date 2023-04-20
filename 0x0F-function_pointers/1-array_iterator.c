26fa419e0cafd92e441bebde3fc78805e6bb8038
kelvin <myemail@gmail.com>
2023-04-20 08:05:29 -0700

    If you spend too much time thinking about a thing, you'll never get it done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - function that executes a function
+ * @action: pointer to a function searching for an integer
+ * @array: an array of integers
+ * @size: size of the array
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+	do {
+		action(*array);
+		array++;
+		size--;
+	} while (size > 0);
+}
