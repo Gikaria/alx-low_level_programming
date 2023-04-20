220c4f330af77521577a958502e2379299c3eb69
kelvin <myemail@gmail.com>
2023-04-20 09:04:45 -0700

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 /**
  * print_name - prints the name as it is
- * @name: the name to print out
- * @f: pointer to function
+ * @name: The name to print out
+ * @f: pointer to function that prints a name
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if (name != NULL &&  f != NULL)
 	{
-		exit(0);
+		f(name);
 	}
-	f(name);
 }
