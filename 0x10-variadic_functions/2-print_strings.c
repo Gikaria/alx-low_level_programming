cc4921a3f9208f4031752b9410b66bef6022e3ea
kelvin <myemail@gmail.com>
2023-04-21 11:53:22 -0700

    One woman's constant is another woman's variable

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_strings - A function thta prints strings
+ * @separator:string to be printed between strings
+ * @n:Number of strings passed to a function
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	char *c;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		c = va_arg(args, char *);
+
+		if (c == NULL)
+		{
+			printf("(nil)");
+		}
+		printf("%s", c);
+		if (separator != NULL && i != n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
